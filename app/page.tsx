c5b20b9e19497e76661c68f2445ec95cbf1198b5
Robert Helmick <robert.helmick.08@gmail.com>
2025-03-23 10:01:36 -0600

    time formatting & header display

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import List from '@/ui/departures-list/list';
+import Icon from '@/ui/icon';
 
 export default function Home() {
   return (
     <div className='items-center'>
-      <h1 className={'text-center'}>Real-time Departure Info</h1>
-      <h3 className={'text-red text-center'}>
-        Route 73 Southbound to Bellevue Station <br />
-        from Quebec & 29th Ave
-      </h3>
+      <h1 className={'text-center'}>Route 73 to Bellevue Station</h1>
+      <div className='flex justify-around'>
+        <Icon></Icon>
+        <div>
+          <h3 className={'text-center'}>from Quebec & 29th Ave</h3>
+          <h3 className={'text-red-600 text-center'}>
+            Real-time Departure Info
+          </h3>
+        </div>
+      </div>
 
       <hr className='solid' />
       <List />
--- ui/departures-list/list-item.tsx
+++ ui/departures-list/list-item.tsx
@@ -3,20 +3,18 @@ import moment from 'moment';
 
 export default function ListItem({ listItem }: { listItem: ScheduleItem }) {
   const time = new Date(listItem.departure_time * 1000);
-  const formattedTime = moment(time).format('hh:mm A');
-
-  const now = moment(new Date()); //todays date
-  const end = moment(time); // another date
+  const now = moment(new Date());
+  const end = moment(time);
   const duration = moment.duration(end.diff(now));
-  console.log('DURATION', duration);
   const minuteDiff = Math.floor(duration.asMinutes());
+  const formattedTime = moment(time).format('hh:mm A');
 
   if (listItem.is_cancelled) {
     return <li className='text-red-600 font-bold'>CANCELLED</li>;
   } else {
     return (
       <li>
-        in {minuteDiff} minutes at {formattedTime}
+        in {minuteDiff >= 0 ? minuteDiff : 0} minutes at {formattedTime}
       </li>
     );
   }
