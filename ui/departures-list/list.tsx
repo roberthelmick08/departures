66bc73e145067b8a952e40bca568c4cf8a73f99c
Robert Helmick <robert.helmick.08@gmail.com>
2025-03-25 14:16:21 -0600

    setting polling interval

--- ui/departures-list/list.tsx
+++ ui/departures-list/list.tsx
@@ -1,9 +1,35 @@
+'use client';
+
 import { fetchDepartures } from '@/lib/data';
 import ListItem from './list-item';
-import { ScheduleItem } from "@/types/types";
+import { ScheduleItem } from '@/types/types';
+import { useState } from 'react';
 
-export default async function List() {
-  const departures: ScheduleItem[] = await fetchDepartures();
+export default function List() {
+  const [departures, setDepartures] = useState<ScheduleItem[]>([]);
+
+  // TODO: use strong typing
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const getDepartures: any = async () => {
+    return await fetchDepartures().then((res: ScheduleItem[]) => {
+      console.log('SET TIMEOUT', res);
+      setDepartures(res);
+    });
+  };
+  const pollDepartures = async () => {
+    getDepartures();
+    const interval = setInterval(async () => {
+      getDepartures().catch((err: Error) => {
+        console.error(err);
+        clearInterval(interval);
+      });
+    }, 30 * 1000);
+  };
+
+  pollDepartures();
+  if (!departures.length) {
+    return;
+  }
 
   return (
     <ul>
