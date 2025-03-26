516e520b48d227f3d43aad851ce1c757e3c8eebb
Robert Helmick <robert.helmick.08@gmail.com>
2025-03-26 09:29:22 -0600

    import env vars for client use

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    API_KEY: process.env.API_KEY,
+  },
 };
 
 export default nextConfig;
